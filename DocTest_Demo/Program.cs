8210e101c60ef1d335cafecad76a14ada4533257
Omer Esin Yildiz <omeresinyildiz@gmail.com>
2023-10-19 13:12:14 +0200

    Create new Image

--- DocTest_Demo/Program.cs
+++ DocTest_Demo/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "CarvedRock.Api", Version = "v1",
+        Description = "The API for the best outdoor recreational gear on the planet!"});;
+} );
 
 var app = builder.Build();
 
